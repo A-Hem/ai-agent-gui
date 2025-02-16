c887f22bf36b7236c427388caec3a8a9650e91b0
A-Hem <154476443+A-Hem@users.noreply.github.com>
2025-02-16 11:12:00 -0600

    Update

---
+++ src/components/APIconfig.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+   import ApiConfig from './components/ApiConfig';
+   import './App.css';
+
+   function App() {
+     return (
+       <div className="App">
+         <ApiConfig />
+         {/* Your existing components */}
+       </div>
+     );
+   }
+
+   export default App;
